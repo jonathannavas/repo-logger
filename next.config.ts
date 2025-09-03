17896a83b4be6020b287cd168601532fa3510ed7
Jonathan Navas <jonathan.navas@ppm.com.ec>
2025-09-03 14:16:25 -0500

    [test] config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
+ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const nextConfig: NextConfig = {
   experimental: {},
   serverExternalPackages: ['sequelize', 'pino', 'pino-pretty'],
 };
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
